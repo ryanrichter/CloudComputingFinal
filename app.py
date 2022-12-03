d09775be25b6bed6cebec5d2062ef4e1aee9e2ec
Jeroen Schickendantz <60529194+Schickendantzj@users.noreply.github.com>
2022-12-03 17:33:57 -0500

    Added DB funtionality

--- app.py
+++ app.py
@@ -1,7 +1,31 @@
 from datetime import datetime
-from flask import Flask, render_template, request, redirect, url_for, send_from_directory
-app = Flask(__name__)
+import os
+from flask import Flask, render_template, request, redirect, url_for, send_from_directory, jsonify
+from flask_cors import CORS
+import pyodbc
 
+app = Flask(__name__)
+CORS(app, resources={r'/*': {'origins': '*'}})
+
+
+cnxn = pyodbc.connect("DRIVER={ODBC Driver 17 for SQL Server};Server=tcp:dbserver-ryanjeroen.database.windows.net,1433;DATABASE=400db;UID=ryanjeroen;PWD=1password!")
+cursor = cnxn.cursor()
+
+
+# Gets HSHD of num
+def get_household(num):
+    rows = cursor.execute("""
+    SELECT HSHD_NUM, BASKET_NUM, PURCHASE, PRODUCT_NUM, SPEND, UNITS, STORE_R, WEEK_NUM, YEAR FROM [400_transactions]
+        WHERE HSHD_NUM = ?
+    """, num).fetchall()
+    out = None
+    if len(rows) > 0:
+        out = {}
+        out["HSHD_NUM"] = num
+        out["columns"] = [t[0] for t in rows[0].cursor_description][1:]
+        print(out["columns"])
+        out["data"] = [list(row)[1:] for row in rows]
+    return out
 
 @app.route('/')
 def index():
@@ -24,6 +48,11 @@ def hello():
        print('Request for hello page received with no name or blank name -- redirecting')
        return redirect(url_for('index'))
 
+@app.route('/data/HSHD/<HSHD_NUM>', methods=['GET'])
+def data(HSHD_NUM):
+    return jsonify(
+        get_household(int(HSHD_NUM))
+    )
 
 if __name__ == '__main__':
    app.run()
